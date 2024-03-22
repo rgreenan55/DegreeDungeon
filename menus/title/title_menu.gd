d4f7e449df1321ef29bc4b26364db2e2cc9be0fe
EthanG78 <ethangarnier78@gmail.com>
2024-03-22 19:45:15 -0300

    Created the scene_transition node that allows for scenes to have a fade in and fade out transition between them

--- menus/title/title_menu.gd
+++ menus/title/title_menu.gd
@@ -2,11 +2,12 @@ extends Control
 class_name TITLE_MENU
 
 @export var starting_scene : PackedScene
+@onready var scene_transition = $SceneTransition
 
 # Start the game at the scene specified in starting_scene
 func _on_play_pressed():
 	GameState.current_scene = starting_scene.resource_path
-	get_tree().change_scene_to_file(GameState.current_scene)
+	scene_transition.transition_to(GameState.current_scene)
 
 # Quit out of the game
 func _on_quit_pressed():
