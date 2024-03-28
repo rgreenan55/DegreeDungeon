25142590192fb234d02010fc21e95c28173ab4a6
ethang <EthanGarnier78@gmail.com>
2024-03-28 11:24:11 -0300

    Created Year 0 (#25)
    * Created the player_home scene which represents the start of the game
    
    * Added better organization to the ui directory. Created a speech_bubble scene that allows for static speech to be rendered in game. Trying to incorporate this into the home scene
    
    * Fixed mom trigger in player_home scene. Designed and added in the mailbox to player_home that will deliver the player their letter.
    
    * Added acceptance letter and animations to UI for player_home scene.
    
    * UI animation updated

---
+++ scenes/world_0/player_home.gd
@@ -0,0 +1,66 @@
+extends Node2D
+
+@onready var ui = %UI
+@onready var player = %Player
+@onready var player_camera = $PlayerCamera
+@onready var mom_speech_bubble = %MomSpeechBubble
+@onready var mom_timer = %MomTimer
+@onready var tile_map = $TileMap
+
+# Variables
+var mom_text_pre_trigger = [
+	"Adammm",
+	"Adam quick come here!",
+	"Adam where are you?"
+]
+
+var mom_text_idx: int = 0
+var mom_text_post_trigger = [
+	"Oh hey honey",
+	"I just saw the mailman out the window",
+	"Can you check the mailbox?",
+	"Thank you!"
+]
+
+var has_adam_triggered_mom: bool = false
+var has_adam_triggered_mail: bool = false
+
+func _ready():
+	# Set max health in GUI to zero since we don't want that to be visible right now
+	ui.set_max_health(0)
+	ui.update_health(0)
+	
+	_update_mom_speech()
+	_update_camera_pos()
+
+func _process(delta):
+	_update_camera_pos()
+
+func _update_camera_pos():
+	player_camera.position.x = player.position.x
+	player_camera.position.y = player.position.y
+	# player_camera.position = clamp(player_camera.position, Vector2(0, 0), Vector2(1152, 648))
+
+func _update_mom_speech():
+	if !has_adam_triggered_mom:
+		# Pull a random text prompt from the pre trigger array as busy talk
+		mom_speech_bubble.set_text(mom_text_pre_trigger.pick_random())
+	elif mom_text_idx < mom_text_post_trigger.size():
+		# Pull a text prompt from the post trigger array in order
+		mom_speech_bubble.set_text(mom_text_post_trigger[mom_text_idx])
+		mom_text_idx += 1
+
+func _on_mom_timer_timeout():
+	_update_mom_speech()
+
+func _on_mom_trigger_body_entered(body):
+	if body.name == "Player" and !has_adam_triggered_mom:
+		# Set the triggered bool
+		has_adam_triggered_mom = true
+		# Remove door to leave house
+		tile_map.set_layer_enabled(2, false)
+
+func _on_mailbox_body_entered(body):
+	if body.name == "Player" and !has_adam_triggered_mail:
+		has_adam_triggered_mail = true
+		ui.send_acceptance_letter()
--- ui/player_ui/ui.gd
+++ ui/player_ui/ui.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 class_name UI
 
-@onready var CoffeeGui = preload("res://ui/scenes/coffee_gui.tscn")
+@onready var CoffeeGui = preload("res://ui/player_ui/scenes/coffee_gui.tscn")
 @onready var health_container = %HealthContainer
 @onready var animation_player = %AnimationPlayer
 @onready var scene_transition = %SceneTransition
@@ -33,9 +33,24 @@ func update_health(current_health: int):
 func player_died():
 	animation_player.play("Fail_Paper_Spin_In")
 	
+func send_acceptance_letter():
+		animation_player.play("Acceptance_Paper_Spin_In")
+	
 func _on_restart_pressed():
 		animation_player.play_backwards("Fail_Paper_Spin_In")
+		await animation_player.animation_finished
 		scene_transition.transition_to(GameState.start_of_year())
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_degree_deny_button_pressed():
+	# If the player has denied the acceptance letter,
+	# then they can just roam around their house indefinetly
+	animation_player.play_backwards("Acceptance_Paper_Spin_In")
+
+func _on_degree_accept_button_pressed():
+	# The player has chosen to accept their degree, starting the game!
+	animation_player.play_backwards("Acceptance_Paper_Spin_In")
+	await animation_player.animation_finished
+	scene_transition.transition_to(GameState.next_scene_path())
