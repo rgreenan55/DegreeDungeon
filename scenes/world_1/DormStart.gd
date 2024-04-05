0e242d3ab7dfef3c32b74d2bd9016e57fcf554ff
ethang <EthanGarnier78@gmail.com>
2024-04-05 17:08:21 -0300

    Develop game levels (#30)
    * Created structure for allowing the hall scene to be the intermediate between each level. Created generic_room_logic gd script that will be used by each room and allow for enemies to be cleared before the player can progress.
    
    * Created Class2 and Class3 that are accessible through the hallway. Fixed some Z-index issues with enemies and projectiles. Fixed nav mesh in map_template_HC13
    
    * Fixed weird collisions on map_template_HC13
    
    * Added class4 and the ITC317 map template
    
    * Added a little dialog to adams dorm.

--- scenes/world_1/DormStart.gd
+++ scenes/world_1/DormStart.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+@onready var speech_bubble = %SpeechBubble
+@onready var room_transition = %RoomTransition
+
+var speech_bubble_text_idx: int = 0
+var speech_bubble_text = [
+	"I could use some caffeine...",
+	"Ahhh, now I'm ready!"
+]
+
 signal s_next_level
 signal s_enable_player
 signal s_enable_ui
@@ -9,6 +18,15 @@ func _ready():
 	s_enable_player.emit()
 	s_enable_ui.emit()
 	s_disable_follow_camera.emit()
+	_display_speech()
 
 func _on_room_transition_s_transition_room():
 	s_next_level.emit()
+
+func _on_health_pickup_picked_up():
+	room_transition.s_transition_room.connect(_on_room_transition_s_transition_room)
+	speech_bubble_text_idx += 1
+	_display_speech()
+	
+func _display_speech():
+	speech_bubble.set_text(speech_bubble_text[speech_bubble_text_idx])
