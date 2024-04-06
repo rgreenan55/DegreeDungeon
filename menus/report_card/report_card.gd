b62627ec03c4d860734f33106814a17792f8a407
EthanG78 <ethangarnier78@gmail.com>
2024-04-06 10:28:48 -0300

    Added the report_card scene that will be displayed after each level. Added sprites for the different grades the player can receive after a level.

---
+++ menus/report_card/report_card.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+@onready var animation_player = %AnimationPlayer
+
+var grades = [
+	"res://menus/common/a_gui.tscn",
+	"res://menus/common/b_gui.tscn",
+	"res://menus/common/c_gui.tscn",
+	"res://menus/common/d_gui.tscn",
+]
