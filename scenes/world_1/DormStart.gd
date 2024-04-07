9d7f1c8c842589d8e158ef11ee00b613bfbb304f
rgreenan55 <rjgreenan55@gmail.com>
2024-04-07 18:49:12 -0300

    ben fix dorm start

--- scenes/world_1/DormStart.gd
+++ scenes/world_1/DormStart.gd
@@ -16,7 +16,7 @@ signal s_enable_player
 signal s_enable_ui
 signal s_disable_follow_camera
 signal s_play_audio(audio_name)
-signal s_show_story_letter(str)
+signal s_show_story_letter(str, title)
 
 func _ready():
 	s_enable_player.emit()
@@ -26,13 +26,13 @@ func _ready():
 
 func _on_room_transition_s_transition_room():
 	s_next_level.emit()
-	s_show_story_letter.emit(story_for_next_level)
+	s_show_story_letter.emit(story_for_next_level, "FIRST CLASS")
 
 func _on_health_pickup_picked_up():
 	s_play_audio.emit("Drinking")
 	room_transition.s_transition_room.connect(_on_room_transition_s_transition_room)
 	speech_bubble_text_idx += 1
 	_display_speech()
-	
+
 func _display_speech():
 	speech_bubble.set_text(speech_bubble_text[speech_bubble_text_idx])
